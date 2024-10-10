3c329907bf78e00225df1a866f3d52838284cc5a
Shrimp_RedCake <nicolebatistabonbon@gmail.com>
2024-10-10 08:35:50 -0300

    womp womp

--- Nova pasta/crs/java.js
+++ Nova pasta/crs/java.js
@@ -1,1 +1,17 @@
+const menu = document.getElementById('menu');
+const toggleBtn = document.getElementById('toggle-btn');
 
+let isOpen = false;
+
+const toggleMenu = () => {
+    if (isOpen) {
+        menu.style.left = '-250px';
+        toggleBtn.style.right = '-50px'; // Botão fora da tela
+    } else {
+        menu.style.left = '0';
+        toggleBtn.style.right = '0'; // Botão visível
+    }
+    isOpen = !isOpen;
+};
+
+toggleBtn.addEventListener('click', toggleMenu);
