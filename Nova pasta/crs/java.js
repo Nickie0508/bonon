16f747b1b1ca849bc79e3a4aae9941a3dc27d1c2
Shrimp_RedCake <nicolebatistabonbon@gmail.com>
2024-10-10 10:10:43 -0300

    test

--- Nova pasta/crs/java.js
+++ Nova pasta/crs/java.js
@@ -1,5 +1,12 @@
 const menu = document.getElementById('menu');
 const toggleBtn = document.getElementById('toggle-btn');
+document.querySelectorAll('.menu a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        alert('Link clicado: ' + e.target.textContent);
+    });
+});
+
 
 let isOpen = false;
 
